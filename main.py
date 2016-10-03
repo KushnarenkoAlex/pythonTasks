7d4ab80d4cbf56a45bbe7c1edb154b6eb7a5b9b0
oleksandr_kushnarenko@epam.com <oleksandr_kushnarenko@epam.com>
2016-10-03 16:51:23 +0300

    task89 done

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from task55 import find_longest_line_in_matrix
 from task62 import check_brackets
+from task89 import replace_spaces
 
 print ("Task 55")
 print (find_longest_line_in_matrix([[0,2,3,4,5,6],[1,0,3,4,5,6],[0,0,0,4,5,6],[1,2,3,0,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6]]))
 print ("Task 65")
 print (check_brackets("5+((1+2)+4)-4+(1+2)"))
 print (check_brackets("5+((((1+2)+4)-4+(1+2)"))
+print ("Task 89")
+print (replace_spaces("One    two    three four"))
